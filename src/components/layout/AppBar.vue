8921b40fc570ef222500c8c6e896715aa157d96b
Marcos Fabian <you@example.com>
2020-08-09 06:47:12 +0800

    addded logo an a few improvements

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -1,15 +1,37 @@
 <template>
-    <v-toolbar>
-        
-    <!-- <div id="nav">
-        <router-link to="/">Homeee</router-link> |
-        <router-link to="/about">About</router-link>
-    </div> -->
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-title>Dr. Peso</v-toolbar-title>
+
+
+      <v-spacer></v-spacer>
+
+    <v-btn to="/" text>
+        <span>Home</span>
+    </v-btn>
+
+    <v-btn to="/profile" text>
+        <span>Login</span>
+    </v-btn>
+
+    <v-btn to="/exchange" text>
+        <span>Best time to exchange?</span>
+    </v-btn>
+
+    <v-btn to="/documentation" text>
+        <span>Documentation</span>
+    </v-btn>
+    
+    <v-btn to="/about" text>
+        <span>About</span>
+    </v-btn>
+      
+    </v-app-bar>
 </template>
 
 <style scoped>
-    #nav {
-    padding: 30px;
-    background-color: black;
-    }
+
 </style>
