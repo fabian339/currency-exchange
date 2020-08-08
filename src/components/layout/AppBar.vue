7dede8ab932aeae1814aa7185e1d5e3059c82975
Marcos Fabian <mrfabian.cs@gmail.com>
2020-08-09 05:14:55 +0800

    started applicaiton

---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-toolbar>
+        
+    <!-- <div id="nav">
+        <router-link to="/">Homeee</router-link> |
+        <router-link to="/about">About</router-link>
+    </div> -->
+</template>
+
+<style scoped>
+    #nav {
+    padding: 30px;
+    background-color: black;
+    }
+</style>
